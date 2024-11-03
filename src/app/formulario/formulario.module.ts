86ee8fc4d4e2b0416e844b17dd2885d8765286c7
Natan Euclides <vnataneuclides1@gmail.com>
2024-11-03 12:52:27 -0300

    first commit

---
+++ src/app/formulario/formulario.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './formulario.page';
+
+import { HomePageRoutingModule } from './formulario-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage]
+})
+export class FormsPageModule {}
---
+++ src/app/formulario/formulario.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'formulario.page.html',
+  styleUrls: ['formulario.page.scss'],
+})
+export class HomePage {
+  // Definindo os dados do formulário
+  formData = {
+    peso: '',
+    velocidadeProducao: '',
+    volume: '',
+    produtosPorMinuto: '',
+    detalhes: ''
+  };
+
+  constructor() {}
+
+  // Função para enviar os dados do formulário a trabalhar
+  submitForm() {
+    console.log(this.formData);
+  }
+}
+
